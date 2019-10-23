cda9201bbb66393bb78ac4fa545dfe458e0fb197
matferronato <mat-ferronato@hotmail.com>
2019-10-23 14:42:22 -0300

    added option to use swp or eth

--- Vagrant/Get_Started/converter_eth.sh
+++ Vagrant/Get_Started/converter_eth.sh
@@ -1,8 +1,8 @@
 
-standartTor="\"tor-!\"   [function=\"leaf\" vagrant=\"eth1\" os=\"hashicorp/bionic64\" version=\"1.0.282\" memory=\"700\" config=\"./helper_scripts/config_production_switch.sh\" ]"
-standartServer="\"server-!\" [function=\"host\" vagrant=\"eth3\" os=\"hashicorp/bionic64\" version=\"1.0.282\" memory=\"700\" config=\"./helper_scripts/config_production_server.sh\" ] "
-standartSwpConnection="\"tor-(\":\"swp&\" -- \"tor-)\":\"swp!\""
-standartServerConnection="\"server-(\":\"eth1\" -- \"tor-)\":\"swp&\" [left_mac=\"00:0!:00:@@:@@:0@\"]"
+standartTor="\"tor-!\" [function=\"host\" vagrant=\"eth1\" os=\"hashicorp/bionic64\" version=\"1.0.282\" memory=\"$3\" config=\"./helper_scripts/config_production_server.sh\" ]"
+standartServer="\"server-!\" [function=\"host\" vagrant=\"eth1\" os=\"hashicorp/bionic64\" version=\"1.0.282\" memory=\"$3\" config=\"./helper_scripts/config_production_server.sh\" ] "
+standartSwpConnection="\"tor-(\":\"eth&\" -- \"tor-)\":\"eth!\" [left_mac=\"00:!0:00:00:00:!!\"][right_mac=\"00:!0:00:00:00:00\"]"
+standartServerConnection="\"server-(\":\"eth1\" -- \"tor-)\":\"eth&\" [left_mac=\"00:0!:00:@@:@@:0@\"][right_mac=\"00:!0:00:@@:@@:0@\"]"
 hatedLine="Vagrant.require_version \">= 1.8.6\", \"< 2.0.0\""
 
 allTors=$1
@@ -25,20 +25,6 @@ for (( everyTor=0; everyTor < $allTors; everyTor++)); do
 done
 
 
-####CREATE SWITCH PORT CONNECTIONS
-currentLetter=A
-currentPort=50
-nextPort=49
-for (( everySwpConnection=0; everySwpConnection < $allSwpConnections; everySwpConnection++)); do
-  nextSwpConnections=$(echo "$currentLetter" | tr "0-9A-Z" "1-9A-Z_")
-  thisSwpConnection=`echo $standartSwpConnection | sed "s/(/$currentLetter/g" | sed "s/)/$nextSwpConnections/g" | sed "s/&/$currentPort/g" | sed "s/!/$nextPort/g"`
-  currentLetter=$(echo "$currentLetter" | tr "0-9A-Z" "1-9A-Z_")
-echo $thisSwpConnection >> topology.dot
-done
-
-echo "" >> topology.dot
-
-
 ####CREATE CONNECTIONS BETWEEN TOR AND SERVERS
 currentLetter=A
 for (( everyTor=0; everyTor < $allTors; everyTor++)); do
@@ -51,7 +37,26 @@ for (( everyTor=0; everyTor < $allTors; everyTor++)); do
   currentLetter=$(echo "$currentLetter" | tr "0-9A-Z" "1-9A-Z_")
 done
 
+echo "" >> topology.dot
+
+####CREATE SWITCH PORT CONNECTIONS
+currentLetter=A
+currentPort=$(($everyServer+1))
+nextPort=$(($everyServer+1))
+for (( everySwpConnection=0; everySwpConnection < $allSwpConnections; everySwpConnection++)); do
+  nextSwpConnections=$(echo "$currentLetter" | tr "0-9A-Z" "1-9A-Z_")
+  thisSwpConnection=`echo $standartSwpConnection | sed "s/(/$currentLetter/g" | sed "s/)/$nextSwpConnections/g" | sed "s/&/$currentPort/g" | sed "s/!/$nextPort/g"`
+  currentLetter=$(echo "$currentLetter" | tr "0-9A-Z" "1-9A-Z_")
+  currentPort=$(($currentPort+1))
+  nextPort=$(($nextPort+1))
+echo $thisSwpConnection >> topology.dot
+done
+
+echo "" >> topology.dot
+
 echo "}" >> topology.dot
+
+
 
 
 python ./topology_converter.py ./topology.dot -p libvirt
